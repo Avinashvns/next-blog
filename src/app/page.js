51c1e508ad074c2842f5852715f0ec4580862836
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-14 00:06:10 +0530

    Event ans Function calling

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,32 @@
+'use client'
 import styles from "./page.module.css";
 
 export default function Home() {
+  // make function
+  const apple=()=>{
+    alert("Fruit")
+  }
+  // param function 
+  const subject=(item)=>{
+    alert(item)
+  }
   return (
-      <main className={styles.main}>
-        <h1>Home Page</h1>
-      </main>
-      
+    <main className={styles.main}>
+      <h1>Home Page</h1>
+
+      <h1>Event Calling</h1>
+      <div>
+        <button onClick={() => alert("Hello Avi")}>Click me</button> 
+      </div>
+  
+      <h1>Function Calling</h1>
+      <div>
+        <button onClick={apple}>Call Function</button>
+      </div>
+      <div>
+        <button onClick={()=>subject("Math")}>Param Function</button>
+      </div>
+    </main>
+
   );
 }
- 
