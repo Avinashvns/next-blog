c3087f25b08a630b8bf12b4ea177d1b5b3dc68ed
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-14 14:27:56 +0530

    Redirection Page
    Redirect from component
    if you open user Page (User Component)
    then you redirect login Page

---
+++ src/app/user/page.js
@@ -0,0 +1,10 @@
+
+import { redirect } from "next/navigation"
+export default function UserPage(){
+    redirect("/login")
+    return(
+        <>
+            <h1>User Page</h1>
+        </>
+    )
+}
