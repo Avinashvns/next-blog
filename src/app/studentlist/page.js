6f04cb51a6d7551bda22e8270b1c1f765e1dfeb9
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-14 13:13:45 +0530

    Dynamic Routes with name ( Params )

--- src/app/studentlist/page.js
+++ src/app/studentlist/page.js
@@ -9,10 +9,10 @@ export default function Studentlist(){
             <h1>Student List </h1>
             <br/><br/>
             <ul style={{marginLeft : "40px"}} >
-                <li><Link href="/studentlist/1" >Avi</Link></li>
-                <li><Link href="/studentlist/2" >Ram</Link></li>
-                <li><Link href="/studentlist/3" >Shyam</Link></li>
-                <li><Link href="/studentlist/4" >Ravan</Link></li>
+                <li><Link href="/studentlist/avi" >Avi</Link></li>
+                <li><Link href="/studentlist/ram" >Ram</Link></li>
+                <li><Link href="/studentlist/shyam" >Shyam</Link></li>
+                <li><Link href="/studentlist/ravan" >Ravan</Link></li>
             </ul>
         </>
     )
