2b4cb8023ce502752dc72055025874717fed4259
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-14 11:26:10 +0530

    Common Layout

---
+++ src/app/login/layout.js
@@ -0,0 +1,11 @@
+export default function Layout({children}){
+    return(
+        <>
+            <h1>Common Layout for Login Page & its all children</h1>
+            <br/>
+            {
+                children
+            }
+        </>
+    )
+}
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,18 @@
+'use client'
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Login(){
+    const router=useRouter()
+    const  navigate=(page)=>{
+        router.push("/login" + page)
+    }
     return(
         <>
             <h1>Login Page</h1>
             <br/><br/>
-            <Link href={"/login/studentlogin"}>Go To Student Login page</Link>
+            {/* <Link href={"/login/studentlogin"}>Go To Student Login page</Link> */}
+            <button onClick={()=>navigate("/studentlogin")}>Function Navigate Login Page</button>
             <br/><br/>
             <Link href={"/"}>Go To Home page</Link>
 
