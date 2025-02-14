4d0713eab9a7040a586f01f8fcb9729acd62bc26
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-14 10:38:54 +0530

    Navigation with function

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,24 @@
 import { useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router=useRouter()
+  const navigate=(name)=>{
+    router.push(name)
+  }
   return(
     <main>
       <h1>Link and Navigation</h1>
       <br />
       <Link href={"/login"}>Go to login page</Link>
+      <br/><br/>
+      <h1>Use Navigation for next page (Navigate)</h1>
+      <br/>
+      <button onClick={()=>router.push("/login")}>Navigate Login Page</button>
+      <br/><br/>
+      <button onClick={()=>navigate("/login")}>Navigate with function to Login Page</button>
     </main>
   )
 }
