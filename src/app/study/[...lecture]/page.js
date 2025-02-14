87849770aaffecee18adf54bc31e333a609a8e1d
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-14 13:55:15 +0530

    Catch all Segment With Multiple Segment
    localhost:3000/study/1/abc

--- src/app/study/[...lecture]/page.js
+++ src/app/study/[...lecture]/page.js
@@ -1,9 +1,13 @@
+'use client'
 
+import { use } from "react"
 
-export default function Lecture(){
+export default function Lecture({params}){
+    const data=use(params)
     return(
         <>
-            <h1> Lecture 1</h1>
+            <h1> Day : {data.lecture[0]}</h1>
+            <h1> Lecture : {data.lecture[1]}</h1>
         </>
     )
 }
