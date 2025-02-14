b6f70f8d2ed563399b1f91228051a6b13a67d928
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-14 14:39:16 +0530

    Redirection from NEXT.Config.mjs
    you can make multiple redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects:async()=>{
+        return [
+            {
+                source: '/user',    // choose Redirection page
+                destination: '/',  // Home Page
+                permanent: false,
+            }
+        ]
+    }
+};
 
 export default nextConfig;
