8ba935f070e25a87209130908d4d35eafe975f0e
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-14 10:19:52 +0530

    Linking

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Login(){
     return(
         <>
             <h1>Login Page</h1>
+            <br/>
+            <Link href={"/"}>Go To Home page</Link>
         </>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,14 @@
 'use client'
 import { useState } from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  const [name,setName]=useState("Avi")
-  // make function
-  const apple=()=>{
-    alert("Fruit")
-  }
-  // param function 
-  const subject=(item)=>{
-    alert(item)
-  }
-  return (
-    <main className={styles.main}>
-      <h1>Home Page with {name}</h1>
-
-      <h1>Event Calling</h1>
-      <div>
-        <button onClick={() => alert("Hello Avi")}>Click me</button> 
-      </div>
-  
-      <h1>Function Calling</h1>
-      <div>
-        <button onClick={apple}>Call Function</button>
-      </div>
-      <div>
-        <button onClick={()=>subject("Math")}>Param Function</button>
-      </div>
+  return(
+    <main>
+      <h1>Link and Navigation</h1>
+      <br />
+      <Link href={"/login"}>Go to login page</Link>
     </main>
-
-  );
+  )
 }
