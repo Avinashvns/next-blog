4de4e07a764fe540d394a2dd2e9d3d3aa14ea4af
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-16 13:51:27 +0530

    Middle ware created

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,7 +1,7 @@
 
 import { redirect } from "next/navigation"
 export default function UserPage(){
-    redirect("/login")
+    // redirect("/login")
     return(
         <>
             <h1>User Page</h1>
